381751f609b8472495e51c20ebc8f65595ec3688
Evgenii <apelvery@gmail.com>
2023-08-13 21:42:54 +0400

    Add logger and communication via Socket

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template_string, send_from_directory, redirect, request
 from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user
 from flask_apscheduler import APScheduler
+from flask_socketio import SocketIO
 
 import serial
 import time
@@ -11,6 +12,9 @@ import numpy as np
 from loguru import logger
 
 
+logger.add("/home/evgenii/plants_final/logs.log", rotation="500 MB", format="{time:YYYY-MM-DD at HH:mm:ss} | {message}")
+
+@logger.catch
 def is_conditioner_on_off_by_photo():
     h1, h2 = 620, 680
     w1, w2 = 210, 260
@@ -36,12 +40,11 @@ class User(UserMixin):
         self.id = id
 
 
-logger.add("/home/evgenii/plants_final/logs.log", rotation="500 MB", format="{time:YYYY-MM-DD at HH:mm:ss} | {message}")
 logger.info("Старт приложения")
 manual_control = False
 
-logger.info("Спим 10 секунд и узнаем статус кондиционера")
-time.sleep(10)
+logger.info("Спим 5 секунд и узнаем статус кондиционера")
+time.sleep(5)
 conditioner_status = is_conditioner_on_off_by_photo()
 logger.info("Статус кондиционера {}", conditioner_status)
 is_rest_need = False
@@ -49,7 +52,7 @@ six_hours = 6 * 60 * 60 # 6 hours
 
 rest_start_time = 0
 rest_time = 60 * 10 # 10 min
-last_turn_off_time = time.time() 
+last_turn_off_time = time.time()
 ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
 
 
@@ -66,6 +69,17 @@ scheduler.api_enabled = True
 scheduler.init_app(app)
 scheduler.start()
 
+socketio = SocketIO(app)
+
+@socketio.on('request_data')
+@logger.catch
+def send_data():
+    global conditioner_status
+    global manual_control
+
+    t, h = read_sensor_data()  # Чтение данных сенсора
+    socketio.emit('update_data', {'t': t, 'h': h, 'conditioner_status': conditioner_status, 'manual_control': manual_control})
+
 def read_sensor_data():
     line = ser.readline()   # читайте строку из последовательного порта
     try:
@@ -75,8 +89,10 @@ def read_sensor_data():
     except:
         t, h = "Error", "Error"
         logger.info("Debug: Температура и Влажность вернули ошибку")
+    #socketio.emit('update_data', {'t': t, 'h': h, 'conditioner_status': conditioner_status, 'manual_control': manual_control})
     return t, h
 
+@logger.catch
 def toggle_conditioner_power():
     max_attempts = 3  # максимальное количество попыток
     attempts = 0
@@ -97,29 +113,31 @@ def toggle_conditioner_power():
     if attempts == max_attempts:
         logger.info("Не удалось изменить состояние кондиционера после максимального количества попыток.")
         return False
-	
+
+@logger.catch
 def is_rest_need_func(conditioner_status):
     global last_turn_off_time
     global rest_start_time
     global six_hours
     current_time = time.time()
-    
+
     # Если кондиционер включен и пришло время для отдыха
     if conditioner_status and current_time - last_turn_off_time > six_hours:
         logger.info("Прошло 6 часов работы кондиционера. Выключаем")
         last_turn_off_time = current_time
         rest_start_time = current_time  # Записываем время начала отдыха
         return True
-    
+
     # Если кондиционер выключен, но уже прошло 10 минут от начала отдыха
     elif not conditioner_status and (current_time - rest_start_time) > (10 * 60):
         logger.info("Прошло 10 минут как кондиционер не работает. Включаем")
         rest_start_time = 0  # Сбросить время начала отдыха
         return False
-    
+
     return None
 
-@scheduler.task('interval', seconds=30, misfire_grace_time=10)
+@scheduler.task('cron', minute="*")
+@logger.catch
 def conditioner_scheduler():
     global conditioner_status
     if manual_control:
@@ -136,13 +154,17 @@ def conditioner_scheduler():
     elif not conditioner_status: # Если текущее время вне диапазона и кондиционер выключен
         logger.info("Время вне диапазона от 4 до 7 утра, включаю кондиционер")
         toggle_conditioner_power()
+    else:
+        logger.info("Time: {}", current_time)
 
 @login_manager.user_loader
+@logger.catch
 def load_user(user_id):
     return User(user_id)
 
 
 @app.route('/login', methods=['GET', 'POST'])
+@logger.catch
 def login():
     if request.method == 'POST':
         username = request.form['username']
@@ -163,19 +185,21 @@ def login():
 
 @app.route('/logout')
 @login_required
+@logger.catch
 def logout():
     logout_user()
     return redirect('/')
 
 @app.route('/')
 @login_required
+@logger.catch
 def index():
     global conditioner_status
     global manual_control
     t, h = read_sensor_data()
     #print(f"Debug: Состояние кондиционера в index: {'Включен' if conditioner_status else 'Выключен'}")
     tz = pytz.timezone('Asia/Tbilisi')
-    
+
     # Получаем текущее время в заданной временной зоне
     current_time = datetime.now(tz)
     formatted_time = current_time.strftime("%Y-%m-%d %H:%M:%S")
@@ -185,7 +209,18 @@ def index():
     <head>
         <title>Image Viewer</title>
         <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/4.0.1/socket.io.js"></script>
         <script>
+            var socket = io.connect('http://' + document.domain + ':' + location.port);
+
+            socket.on('update_data', function(data) {
+                document.getElementById("time").innerHTML = "Текущее время: " + data.formatted_time;
+                document.getElementById("temperature").innerHTML = "Температура: " + data.t + " °C";
+                document.getElementById("humidity").innerHTML = "Влажность: " + data.h + " %";
+                document.getElementById("conditioner_status").innerHTML = "Состояние кондиционера: " + (data.conditioner_status ? "Включен" : "Выключен");
+                document.getElementById("control_mode").innerHTML = "Режим управления: " + (data.manual_control ? "Ручное Управление" : "Автоматическое Управление");
+            });
+
             function updateImage() {
                 // Меняем URL изображения, добавляя временную метку, чтобы избежать кеширования
                 $('#image').attr('src', '/image?timestamp=' + new Date().getTime());
@@ -197,11 +232,11 @@ def index():
     </head>
     <body>
         <img id="image" src="/image" width="480" height="720"><br>
-        <p>Текущее время: {{ formatted_time }}</p>
-        <p>Температура: {{ t }} °C</p>
-        <p>Влажность: {{ h }} %</p>
-        <p>Состояние кондиционера: {{ "Включен" if conditioner_status else "Выключен" }}</p>
-        <p>Режим управления: {{ "Ручное Управление" if manual_control else "Автоматическое Управление" }}</p>
+        <p id="time">Текущее время: {{ formatted_time }}</p>
+        <p id="temperature">Температура: {{ t }} °C</p>
+        <p id="humidity">Влажность: {{ h }} %</p>
+        <p id="conditioner_status">Состояние кондиционера: {{ "Включен" if conditioner_status else "Выключен" }}</p>
+        <p id="control_mode">Режим управления: {{ "Ручное Управление" if manual_control else "Автоматическое Управление" }}</p>
         <button onclick="location.href='/set_manual_control'">Переключить на {{ "Ручное Управление" if not manual_control else "Автоматическое Управление" }}</button>
         <button onclick="location.href='/control?cmd=power'">Включить/Выключить</button>
         <button onclick="location.href='/control?cmd=up'">Повысить температуру</button>
@@ -213,6 +248,7 @@ def index():
 
 @app.route('/control')
 @login_required
+@logger.catch
 def control():
     cmd = request.args.get('cmd')
     if cmd == 'power':
@@ -229,22 +265,24 @@ def control():
     elif cmd == 'fan':
         ser.write(b'S')
 
-	# Вернуться обратно на главную страницу
+        # Вернуться обратно на главную страницу
     return redirect('/')
 
 @app.route('/set_manual_control')
 @login_required
+@logger.catch
 def set_manual_control():
     global manual_control
     logger.info("Меняю режим управления")
     manual_control = not manual_control
-    logger.info(f"Режим управления {'Manual' if manual_control else 'Auto'}") 
+    logger.info(f"Режим управления {'Manual' if manual_control else 'Auto'}")
     return redirect('/')
 
 @app.route('/image')
 @login_required
+@logger.catch
 def serve_image():
     return send_from_directory('/home/evgenii/plants_final', 'image.jpg')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    socketio.run(app, host='0.0.0.0', port=5000)
